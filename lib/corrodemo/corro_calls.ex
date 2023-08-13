81a79c6a7ec19d8a0933f22949d982c0bdce0d6b
Chris Nicoll <c@fly.io>
2023-08-13 11:54:52 -0400

    tidy corrosion request functions

--- lib/corrodemo/corro_calls.ex
+++ lib/corrodemo/corro_calls.ex
@@ -8,32 +8,25 @@ defmodule Corrodemo.CorroCalls do
     corro_db_url = "#{Application.fetch_env!(:corrodemo, :corro_baseurl)}/v1/"
     with {:ok, %Finch.Response{status: status_code, body: body, headers: headers}} <- Finch.build(:post,"#{corro_db_url}#{path}",[{"content-type", "application/json"}],Jason.encode!(statement))
       |> Finch.request(Corrodemo.Finch) do
-        {:ok, %{status_code: status_code, body: body, headers: headers}}
-      else
-        {:error, resp} -> {:error, resp}
-        another_response -> IO.puts("corro_request: response has an unexpected format")
-          inspect(another_response) |> IO.inspect(label: "Got response")
+        case status_code do
+          200 -> extract_results(body)
+          404 -> IO.inspect("got a 404")
+          422 -> IO.puts("got a 422 -- check syntax and json formatting")
+          unexpected_response -> inspect(unexpected_response) |> IO.inspect(label: "got a non-200, non-404 error code")
+        end
+    else
+      {:ok, response} -> IO.inspect(response, label: "Got an unexpected response in query_corro")
+      {:error, resp} -> {:error, resp}
+      another_response -> inspect(another_response) |> IO.inspect(label: "corro_request: response has an unexpected format")
     end
   end
 
   def execute_corro(statement) do
-    with {:ok, response} <- corro_request("transactions", statement) do
-      # IO.puts("got an ok, response from corro_request")
-      # inspect(response) |> IO.inspect(label: "the response")
-      # IO.inspect(Map.get(response, :status_code), label: "status_code")
-      case response[:status_code] do
-        200 -> extract_results(response[:body])
-        404 -> IO.inspect("got a 404")
-        unexpected_response -> IO.inspect(unexpected_response, label: "got a non-200, non-404 error code")
-      end
-    end
+    corro_request("transactions", statement)
   end
 
   def query_corro(statement) do
-    with {:ok, %{body: body, headers: headers, status_code: 200}} <- corro_request("queries", statement),
-      {:ok, results} <- extract_results(body) do
-          {:ok, results}
-    end
+    corro_request("queries", statement)
   end
 
   defp extract_results(body) do
